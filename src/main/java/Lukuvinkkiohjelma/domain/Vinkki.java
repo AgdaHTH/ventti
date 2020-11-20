d0a2ca7bac3b0c5d38a2ced75d3471d133fea75e
Mari Karlsson <59450590+mkkarl@users.noreply.github.com>
2020-11-20 18:05:56 +0200

    Sovelluslogiikkaan ja Vinkkiin lisätty vinkin lisäys

--- src/main/java/Lukuvinkkiohjelma/domain/Vinkki.java
+++ src/main/java/Lukuvinkkiohjelma/domain/Vinkki.java
@@ -10,9 +10,10 @@ package Lukuvinkkiohjelma.domain;
  * @author mari
  */
 public class Vinkki {
+
     private String otsikko;
     private String tyyppi;
-    
+
     public Vinkki(String otsikko, String tyyppi) {
         this.otsikko = otsikko;
         this.tyyppi = tyyppi;
