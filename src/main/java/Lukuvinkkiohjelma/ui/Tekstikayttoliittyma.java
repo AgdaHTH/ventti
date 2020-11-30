f4c1665408d27ef21f9fa2348f8627f0c97947ec
Jeemlei <eemeli.ingervo@gmail.com>
2020-11-30 17:30:49 +0200

    Switch-case else-if tilalle

--- src/main/java/Lukuvinkkiohjelma/ui/Tekstikayttoliittyma.java
+++ src/main/java/Lukuvinkkiohjelma/ui/Tekstikayttoliittyma.java
@@ -45,80 +45,72 @@ public class Tekstikayttoliittyma {
             String komento = io.readLine("Anna komento");
             io.print("\nTyhjä syöte eli pelkkä enter keskeyttää toiminnon.\n");
 
-            if (komento.equals("1")) {
-
-                io.print("Millainen vinkki lisätään?\n\t1. Kirja\n\t2. Podcast\n\t3. Blogi\n");
-                String vinkkityyppi = io.readLine("Anna komento");
-                io.print("");
-
-                if (keskeytetaan(vinkkityyppi)) {
-                    continue;
-                }
-
-                if (vinkkityyppi.equals("1")) {
-                    String otsikko = io.readLine("Kirjan otsikko: ");
-                    if (keskeytetaan(otsikko)) {
+            switch (komento) {
+                case "1":
+                    io.print("Millainen vinkki lisätään?\n\t1. Kirja\n\t2. Podcast\n\t3. Blogi\n");
+                    String vinkkityyppi = io.readLine("Anna komento");
+                    io.print("");
+                    if (keskeytetaan(vinkkityyppi)) {
                         continue;
+                    }   if (vinkkityyppi.equals("1")) {
+                        String otsikko = io.readLine("Kirjan otsikko: ");
+                        if (keskeytetaan(otsikko)) {
+                            continue;
+                        }
+                        
+                        String kirjoittaja = io.readLine("\nKirjan kirjoittaja: ");
+                        if (keskeytetaan(kirjoittaja)) {
+                            continue;
+                        }
+                        String ISBN = io.readLine("\nKirjan ISBN: ");
+                        if (keskeytetaan(ISBN)) {
+                            continue;
+                        }
+                        
+                        if (sovelluslogiikka.lisaaVinkki(new Kirja(otsikko, kirjoittaja, ISBN))) {
+                            io.print("");
+                            io.print("Uusi kirjavinkki lisatty onnistuneesti!");
+                            io.print("");
+                        } else {
+                            io.print("\nJotain meni pieleen uuden kirjavinkin lisamisessa...\n");
+                        }
+                    }   break;
+                case "2":
+                    {
+                        List<Kirja> kirjalista = sovelluslogiikka.listaaKirjat();
+                        // Tähän myös podcastien ja blogien haku...
+                        if (!kirjalista.isEmpty()) {
+                            for (Kirja kirja : kirjalista) {
+                                io.print(kirja.toString());
+                            }
+                        } else {
+                            io.print("Ei vielä lisättyjä vinkkejä!\n");
+                        }       break;
                     }
-
-                    String kirjoittaja = io.readLine("\nKirjan kirjoittaja: ");
-                    if (keskeytetaan(kirjoittaja)) {
-                        continue;
+                case "3":
+                    {
+                        List<Kirja> kirjalista = sovelluslogiikka.listaaKirjat();
+                        if (!kirjalista.isEmpty()) {
+                            for (int i = 0; i < kirjalista.size(); i++) {
+                                io.print(i + " " + kirjalista.get(i));
+                            }
+                        }       String numeroString = io.readLine("Anna poistettavan vinkin numero:");
+                        if (keskeytetaan(numeroString)) {
+                            continue;
+                        }       int numero = Integer.parseInt(numeroString);
+                        if (sovelluslogiikka.poistaVinkki(kirjalista.get(numero))) {
+                            io.print("\nVinkki poistettu onnistuneesti!\n");
+                        } else {
+                            io.print("\nJotain meni pieleen vinkin poistamisessa.\n");
+                        }       break;
                     }
-                    String ISBN = io.readLine("\nKirjan ISBN: ");
-                    if (keskeytetaan(ISBN)) {
-                        continue;
-                    }
-
-                    if (sovelluslogiikka.lisaaVinkki(new Kirja(otsikko, kirjoittaja, ISBN))) {
-                        io.print("");
-                        io.print("Uusi kirjavinkki lisatty onnistuneesti!");
-                        io.print("");
-                    } else {
-                        io.print("\nJotain meni pieleen uuden kirjavinkin lisamisessa...\n");
-                    }
-                }
-
-            } else if (komento.equals("2")) {
-                List<Kirja> kirjalista = sovelluslogiikka.listaaKirjat();
-                // Tähän myös podcastien ja blogien haku...
-
-                if (!kirjalista.isEmpty()) {
-                    for (Kirja kirja : kirjalista) {
-                        io.print(kirja.toString());
-                    }
-                } else {
-                    io.print("Ei vielä lisättyjä vinkkejä!\n");
-                }
-
-            } else if (komento.equals("3")) {
-                List<Kirja> kirjalista = sovelluslogiikka.listaaKirjat();
-
-                if (!kirjalista.isEmpty()) {
-                    for (int i = 0; i < kirjalista.size(); i++) {
-                        io.print(i + " " + kirjalista.get(i));
-                    }
-                }
-
-                String numeroString = io.readLine("Anna poistettavan vinkin numero:");
-
-                if (keskeytetaan(numeroString)) {
-                    continue;
-                }
-                
-                int numero = Integer.parseInt(numeroString);
-
-                if (sovelluslogiikka.poistaVinkki(kirjalista.get(numero))) {
-                    io.print("\nVinkki poistettu onnistuneesti!\n");
-                } else {
-                    io.print("\nJotain meni pieleen vinkin poistamisessa.\n");
-                }
-
-            } else if (komento.equals("0")) {
-                io.print("Kiitos käynnistä! Hei hei!");
-                kaynnissa = false;
-            } else {
-                io.print("Komento tuntematon, syötä uusi komento.\n");
+                case "0":
+                    io.print("Kiitos käynnistä! Hei hei!");
+                    kaynnissa = false;
+                    break;
+                default:
+                    io.print("Komento tuntematon, syötä uusi komento.\n");
+                    break;
             }
         }
     }
