6e78a51197cb2090b5a417af3715af9c2ea00f39
Jeemlei <eemeli.ingervo@gmail.com>
2020-11-30 17:28:50 +0200

    Blogi ja podcast sovelluslogiikkaan

--- src/main/java/Lukuvinkkiohjelma/domain/Sovelluslogiikka.java
+++ src/main/java/Lukuvinkkiohjelma/domain/Sovelluslogiikka.java
@@ -15,14 +15,14 @@ import java.util.List;
  * @author mari
  */
 public class Sovelluslogiikka {
+
     // JSON-raakadatalista:
     private List<Object> vinkkilista;
-    // Kirjavinkkilista:
+
     private List<Kirja> kirjalista;
-    
-    // private List<Podcast> podcastlista;
-    // private List<Blogi> blogilista;
-    
+    private List<Podcast> podcastlista;
+    private List<Blogi> blogilista;
+
     private VinkkiDao dao;
 
     public Sovelluslogiikka(VinkkiDao vinkkiDao) {
@@ -41,40 +41,74 @@ public class Sovelluslogiikka {
     public boolean lisaaVinkki(Object vinkki) {
         if (vinkki instanceof Kirja) {
             this.kirjalista.add((Kirja) vinkki);
+        } else if (vinkki instanceof Podcast) {
+            this.podcastlista.add((Podcast) vinkki);
+        } else if (vinkki instanceof Blogi) {
+            this.blogilista.add((Blogi) vinkki);
         }
-        
-        // elseif instanceof podcast...
-        
+
         // Daolle object-muodossa tallennettavaksi
         return this.dao.lisaaVinkki(vinkki);
     }
 
-    // Metodi listaa kaikki tallennetut vinkit tarkastelua varten
     public List<Kirja> listaaKirjat() {
-        List<Object> poistettavatjsonit = new ArrayList<>();
-        
-        if (!vinkkilista.isEmpty()) {
-            Gson gson = new Gson();
-            
-            // Luupin tulee ottaa huomioon myös Podcast ja Blogityypit!
-            for (Object jsonvinkki : vinkkilista) {
-                // fromJson kaataa ohjelman, jos .toJsonia ei ole ja missä tahansa
-                // kentässä on välilyönti!
-                Kirja kirja = gson.fromJson(gson.toJson(jsonvinkki), Kirja.class);
-                kirjalista.add(kirja);
-                poistettavatjsonit.add(jsonvinkki);
-            }
-            
-            vinkkilista.removeAll(poistettavatjsonit);
-        }
-        
+        parsiVinkitListoille();
         return kirjalista;
+    }
+
+    public List<Podcast> listaaPodcastit() {
+        parsiVinkitListoille();
+        return podcastlista;
+    }
+
+    public List<Blogi> listaaBlogit() {
+        parsiVinkitListoille();
+        return blogilista;
+    }
+
+    // Metodi listaa kaikki tallennetut vinkit tarkastelua varten
+    private boolean parsiVinkitListoille() {
+        List<Object> poistettavatjsonit = new ArrayList<>();
+
+        try {
+            if (!vinkkilista.isEmpty()) {
+                Gson gson = new Gson();
+
+                for (Object jsonvinkki : vinkkilista) {
+                    // fromJson aiheuttaa virheen, jos .toJsonia ei ole ja missä tahansa
+                    // kentässä on välilyönti!
+                    if (jsonvinkki instanceof Kirja) {
+                        Kirja kirja = gson.fromJson(gson.toJson(jsonvinkki), Kirja.class);
+                        kirjalista.add(kirja);
+                    } else if (jsonvinkki instanceof Podcast) {
+                        Podcast podcast = gson.fromJson(gson.toJson(jsonvinkki), Podcast.class);
+                        podcastlista.add(podcast);
+                    } else if (jsonvinkki instanceof Blogi) {
+                        Blogi blogi = gson.fromJson(gson.toJson(jsonvinkki), Blogi.class);
+                        blogilista.add(blogi);
+                    }
+                    poistettavatjsonit.add(jsonvinkki);
+                }
+
+                vinkkilista.removeAll(poistettavatjsonit);
+            }
+        } catch (Exception e) {
+            System.out.println("Listauksessa tapahtui virhe:\n");
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     // Tällä hetkellä (29.11.) poistaa vain paikallisesta listasta.
     public boolean poistaVinkki(Object vinkki) {
         if (vinkki instanceof Kirja) {
             kirjalista.remove((Kirja) vinkki);
+        } else if (vinkki instanceof Podcast) {
+            podcastlista.remove((Podcast) vinkki);
+        } else if (vinkki instanceof Blogi) {
+            blogilista.remove((Blogi) vinkki);
         }
         return this.dao.poistaVinkki(vinkki);
     }
