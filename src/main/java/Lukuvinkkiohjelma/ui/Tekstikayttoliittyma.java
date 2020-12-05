c1db99e5150eedcb361f92edd46c6e974d8b9b0c
Juha Kauppinen <juha.kauppinen@helsinki.fi>
2020-12-05 14:51:04 +0200

    Tekstikäyttöliittymää pilkottu, toisteista koodia poistettu

--- src/main/java/Lukuvinkkiohjelma/ui/Tekstikayttoliittyma.java
+++ src/main/java/Lukuvinkkiohjelma/ui/Tekstikayttoliittyma.java
@@ -39,111 +39,38 @@ public class Tekstikayttoliittyma {
         while (kaynnissa) {
 
             io.print("Komennot:");
-            io.print("1 - Lisää vinkki");
-            io.print("2 - Listaa vinkit");
+            io.print("1 - Listaa vinkit");
+            io.print("2 - Lisää vinkki");
             io.print("3 - Poista vinkki");
+            //io.print("3 - Muokkaa vinkkiä");
+            //io.print("5 - Muokkaa listauksen parametreja");
             io.print("0 - Sulje ohjelma");
             io.print("");
 
             String komento = io.readLine("Anna komento");
             io.print("\nTyhjä syöte eli pelkkä enter keskeyttää toiminnon.\n");
 
+            List<Vinkki> vinkit;
+            
             switch (komento) {
                 case "1":
-                    io.print("Millainen vinkki lisätään?\n\t1. Kirja\n\t2. Podcast\n\t3. Blogi\n");
-                    String vinkkityyppi = io.readLine("Anna komento");
-                    io.print("");
-                    if (keskeytetaan(vinkkityyppi)) {
-                        continue;
-                    }   if (vinkkityyppi.equals("1")) {
-                        String otsikko = io.readLine("Kirjan otsikko: ");
-                        if (keskeytetaan(otsikko)) {
-                            continue;
-                        }
-                        
-                        String kirjoittaja = io.readLine("\nKirjan kirjoittaja: ");
-                        if (keskeytetaan(kirjoittaja)) {
-                            continue;
-                        }
-                        String ISBN = io.readLine("\nKirjan ISBN: ");
-                        if (keskeytetaan(ISBN)) {
-                            continue;
-                        }
-                        
-                        if (sovelluslogiikka.lisaaVinkki(new Kirja(otsikko, kirjoittaja, ISBN))) {
-                            io.print("");
-                            io.print("Uusi kirjavinkki lisatty onnistuneesti!");
-                            io.print("");
-                        } else {
-                            io.print("\nJotain meni pieleen uuden kirjavinkin lisamisessa...\n");
-                        }
-                    } else if (vinkkityyppi.equals("2")) {
-                        String otsikko = io.readLine("Podcastin otsikko: ");
-                        if (keskeytetaan(otsikko)) {
-                            continue;
-                        }
-
-                        String Url = io.readLine("\nPodcastin Url: ");
-                        if (keskeytetaan(Url)) {
-                            continue;
-                        }
-                        
-                        if (sovelluslogiikka.lisaaVinkki(new Podcast(otsikko, Url))) {
-                            io.print("");
-                            io.print("Uusi podcastvinkki lisatty onnistuneesti!");
-                            io.print("");
-                        } else {
-                            io.print("\nJotain meni pieleen uuden blogivinkin lisamisessa...\n");
-                        }  break;
-                    }
-                    else if (vinkkityyppi.equals("3")) {
-                        String otsikko = io.readLine("Blogin otsikko: ");
-                        if (keskeytetaan(otsikko)) {
-                            continue;
-                        }
-                        
-                        String kirjoittaja = io.readLine("\nBlogin kirjoittaja: ");
-                        if (keskeytetaan(kirjoittaja)) {
-                            continue;
-                        }
-                        String Url = io.readLine("\nBlogin Url: ");
-                        if (keskeytetaan(Url)) {
-                            continue;
-                        }
-                        
-                        if (sovelluslogiikka.lisaaVinkki(new Blogi(otsikko, kirjoittaja, Url))) {
-                            io.print("");
-                            io.print("Uusi blogivinkki lisatty onnistuneesti!");
-                            io.print("");
-                        } else {
-                            io.print("\nJotain meni pieleen uuden blogivinkin lisamisessa...\n");
-                        } break;
-                    }
+                    vinkit = sovelluslogiikka.listaaKaikkiVinkit();
+                    listaaVinkit(vinkit);
+                    break;
                 case "2":
-                    {
-                        List<Vinkki> vinkit = sovelluslogiikka.listaaKaikkiVinkit();
-                        if (!vinkit.isEmpty()) {
-                            for (Vinkki vinkki : vinkit) {
-                                io.print(vinkki.toString());
-                            }
-                        } else {
-                            io.print("Ei vielä lisättyjä vinkkejä!\n");
-                        } break;
-                    }
+                    lisaaVinkki();
+                    break;
                 case "3":
                     {
-                        List<Vinkki> vinkit = sovelluslogiikka.listaaKaikkiVinkit();
-                        if (!vinkit.isEmpty()) {
-                            for (int i = 0; i < vinkit.size(); i++) {
-                                io.print(i + " " + vinkit.get(i));
-                            }
-                        }       
+                        vinkit = sovelluslogiikka.listaaKaikkiVinkit();
+                        listaaVinkit(vinkit);
+                             
                         String numeroString = io.readLine("Anna poistettavan vinkin numero:");
                         if (keskeytetaan(numeroString)) {
                             continue;
                         }       
                         int indeksi = Integer.parseInt(numeroString);
-                        if (sovelluslogiikka.poistaVinkki(indeksi)) {
+                        if (sovelluslogiikka.poistaVinkki(indeksi - 1)) {
                             io.print("\nVinkki poistettu onnistuneesti!\n");
                         } else {
                             io.print("\nJotain meni pieleen vinkin poistamisessa.\n");
@@ -159,6 +86,88 @@ public class Tekstikayttoliittyma {
             }
         }
     }
+    
+    // Uuden vinkin lisääminen
+    private void lisaaVinkki() {
+        io.print("Millainen vinkki lisätään?\n\t1. Kirja\n\t2. Podcast\n\t3. Blogi\n");
+        String vinkkityyppi = io.readLine("Anna komento");
+        io.print("");
+        if (keskeytetaan(vinkkityyppi)) {
+            return;
+        }   
+        switch (vinkkityyppi) {
+            case "1":
+                {
+                    String otsikko = io.readLine("Kirjan otsikko: ");
+                    if (keskeytetaan(otsikko)) {
+                        return;
+                    }
+                    String kirjoittaja = io.readLine("\nKirjan kirjoittaja: ");
+                    if (keskeytetaan(kirjoittaja)) {
+                        //continue;
+                    }
+                    String ISBN = io.readLine("\nKirjan ISBN: ");
+                    if (keskeytetaan(ISBN)) {
+                        return;
+                    }
+                    talletaVinkki(new Kirja(otsikko, kirjoittaja, ISBN));
+                    
+                    break;
+                }
+            case "2":
+                {
+                    String otsikko = io.readLine("Podcastin otsikko: ");
+                    if (keskeytetaan(otsikko)) {
+                        return;
+                    }
+                    String Url = io.readLine("\nPodcastin Url: ");
+                    if (keskeytetaan(Url)) {
+                        return;
+                    }
+                    talletaVinkki(new Podcast(otsikko, Url));
+
+                    break;
+                }
+            case "3":
+                {
+                    String otsikko = io.readLine("Blogin otsikko: ");
+                    if (keskeytetaan(otsikko)) {
+                        //continue;
+                    }
+                    String kirjoittaja = io.readLine("\nBlogin kirjoittaja: ");
+                    if (keskeytetaan(kirjoittaja)) {
+                        return;
+                    }
+                    String Url = io.readLine("\nBlogin Url: ");
+                    if (keskeytetaan(Url)) {
+                        return;
+                    }
+                    talletaVinkki(new Blogi(otsikko, kirjoittaja, Url));
+                    
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+    
+    private void talletaVinkki(Vinkki vinkki) {
+        if (sovelluslogiikka.lisaaVinkki(vinkki)) {
+            io.print("\nUusi " + vinkki.getClass().getSimpleName() + "vinkki" + " lisatty onnistuneesti!\n");
+        } else {
+            io.print("\nJotain meni pieleen uuden kirjavinkin lisamisessa...\n");
+        }
+    }
+    
+    private void listaaVinkit(List<Vinkki> vinkit) {
+        if (!vinkit.isEmpty()) {
+            for (int i = 1; i < vinkit.size(); i++) {
+                io.print(i + " " + vinkit.get(i));
+            }
+        } else {
+            io.print("Ei vielä lisättyjä vinkkejä!\n");
+        }
+    }
 
     private boolean keskeytetaan(String syote) {
         if (syote.equals("")) {
@@ -168,4 +177,16 @@ public class Tekstikayttoliittyma {
 
         return false;
     }
+    
+    private int lueLukuValilta(String prompt, int suurin) {
+        while(true) {            
+            int luku = io.readInt(prompt);
+            if (luku <= suurin) {
+               return luku; 
+            } else {
+                io.print("Virheellinen syöte!\n");
+            }
+        }
+    }
+   
 }
